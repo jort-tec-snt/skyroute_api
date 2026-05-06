c295430316b229aa43eb6238807d02e2663fb1af
Jeronimo Ortiz <jeronimo.ortiz@tecsup.edu.pe>
2026-05-06 12:20:27 -0500

    Añadir conjuntos de vistas con CRUD y filtro de búsqueda

--- flights/views.py
+++ flights/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import viewsets, filters
+from .models import Airline, Flight
+from .serializers import AirlineSerializer, FlightSerializer
 
-# Create your views here.
+
+class AirlineViewSet(viewsets.ModelViewSet):
+    queryset = Airline.objects.all().order_by('id')
+    serializer_class = AirlineSerializer
+
+
+class FlightViewSet(viewsets.ModelViewSet):
+    queryset = Flight.objects.all().order_by('id')
+    serializer_class = FlightSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['origin', 'destination', 'code']
