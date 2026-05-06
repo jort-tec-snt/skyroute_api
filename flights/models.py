2c9c97ccd33d6330ce80ad4fd7f7513d693637b1
Jeronimo Ortiz <jeronimo.ortiz@tecsup.edu.pe>
2026-05-06 12:17:26 -0500

    Agregar modelos de aerolíneas y vuelos con relación

--- flights/models.py
+++ flights/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Airline(models.Model):
+    name = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Flight(models.Model):
+    code = models.CharField(max_length=20, unique=True)
+    origin = models.CharField(max_length=100)
+    destination = models.CharField(max_length=100)
+    airline = models.ForeignKey(
+        Airline,
+        on_delete=models.CASCADE,
+        related_name='flights'
+    )
+
+    def __str__(self):
+        return self.code
